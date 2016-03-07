daf616bb1d529f3cebd970000a081555e12f42eb
r-moric <rmoric@gmail.com>
2016-03-07 00:16:20 -0500

    Added files via upload

--- numerai4_h2o_simp_ensemble.R
+++ numerai4_h2o_simp_ensemble.R
@@ -5,6 +5,20 @@ library(h2oEnsemble)
 h2o.removeAll() # Clean up. Just in case H2O was already running
 h2o.init(nthreads = -1, max_mem_size="22G")  # Start an H2O cluster with all threads available
 
+# Get training and tournament data -------------------
+train <- read.csv("http://datasets.numer.ai/numerai_training_data.csv")
+tourn <- read.csv("http://datasets.numer.ai/numerai_tournament_data.csv")
+
+# Convert target to factor -------------------
+train$target <- as.factor(train$target)
+
+# Get feature names -------------------
+feature_names <- names(train)[1:(ncol(train)-1)]
+
+# Convert to H20 objects -------------------
+train_h2o <- as.h2o(train)
+tourn_h2o <- as.h2o(tourn)
+
 # Setup learners --------------------
 learner <- c("h2o.glm.wrapper", "h2o.randomForest.wrapper", "h2o.gbm.wrapper", "h2o.deeplearning.wrapper") #
 metalearner <- "h2o.glm.wrapper"
@@ -21,9 +35,9 @@ model_ens_simp <- h2o.ensemble(x = feature_names,
 
 
 pred_ens_simp <- predict(model_ens_simp, tourn_h2o)
-predictions <- as.data.frame(pred_ens_simp$pred)[,3]  #third column is P(Y==1)
-prob <- cbind(prob, predictions)
-write.table(prob[, c("t_id", "predictions")], paste0("h2o_simp_ens_CV10", ".csv"), sep = ",", row.names = FALSE, col.names = c("t_id", "probability"))
+pred <- as.data.frame(pred_ens_simp$pred)[,3]  #third column is P(Y==1)
+prob <- cbind(tourn[, c("t_id"), drop = FALSE], probability = pred)
+write.table(prob[, c("t_id", "probability")], paste0("h2o_simp_ens_CV10", ".csv"), sep = ",", row.names = FALSE)
 # LOGLOSS @ NUMERAI - 0.6912
 
 # Shutdown H2O --------------------
